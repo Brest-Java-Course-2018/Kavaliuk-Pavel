d1e6d214556ec4cb8622613f9c03fc7daa91dbd1
Kavaliuk-Pavel <voorhis365@mail.ru>
2018-02-13 17:22:01 +0300

    Added new testclass HelloWorldTest, which compare needed string with obtained.

---
+++ src/test/java/sampleuserapp/HelloWorldTest.java
@@ -0,0 +1,41 @@
+package sampleuserapp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    private static String args[];
+
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    /*
+    This method will be invoked before procedure which annotated like @Test.
+    It's need for catch output stream.
+    */
+    @Before
+    public void setUpStream(){
+        System.setOut(new PrintStream(outContent));
+    }
+    /*
+    This method will be invoked after procedure which annotated like @Test.
+    It's need to return output stream.
+     */
+    @After
+    public void repairStream(){
+        System.setOut(System.out);
+    }
+
+    /*
+    This procedure compares needed string with obtained.
+     */
+    @Test
+    public void returnTest(){
+        App.main(args);
+        assertEquals("Hello World!\n", outContent.toString());
+    }
+}
