1249a84fe6d52dce3512e9f27dc60bd5abbac9cd
WonderCat365 <voorhis365@mail.ru>
2018-03-24 20:36:11 +0300

    New Project.

--- test-db/src/main/resources/data-script.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO department (departmentName, description)
-VALUES ('Distribution', 'Distribution Department');
-
-INSERT INTO employees (employeeName, salary, departmentFatherId, employeeEmail)
-VALUES ('Ivan Ivanov', 300, 1, 'voorhis365@mail.ru');
